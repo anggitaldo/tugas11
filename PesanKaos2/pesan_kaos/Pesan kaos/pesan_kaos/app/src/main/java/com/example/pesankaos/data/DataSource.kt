e78a9aa18d0d04d8deb42d2834fbdc709fcd65c1
Anggit Aldo Aryadika <anggitaldo05@gmail.com>
2025-07-07 21:30:20 +0700

    the commit

---
+++ PesanKaos2/pesan_kaos/Pesan kaos/pesan_kaos/app/src/main/java/com/example/pesankaos/data/DataSource.kt
@@ -0,0 +1,17 @@
+package com.example.pesankaos.data
+import com.example.pesankaos.R
+object DataSource {
+    val colors = listOf(
+        R.string.putih,
+        R.string.merah,
+        R.string.biru,
+        R.string.hijau,
+        R.string.coklat,
+        R.string.hitam
+    )
+    val quantityOptions = listOf(
+        Pair(R.string.satu_kaos, 1),
+        Pair(R.string.enam_kaos, 6),
+        Pair(R.string.duabelas_kaos, 12)
+    )
+}
---
+++ PesanKaos2/pesan_kaos/app/src/main/java/com/example/pesankaos/data/TabBarUiState.kt
@@ -0,0 +1,10 @@
+package com.example.pesankaos.data
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class TabBarUiState(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val badgeCount: Int? = null
+)
